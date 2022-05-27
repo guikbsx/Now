2c0b58fa55b01b3a23b5da478ad8c7b17df4ef38
Guillaume Bisiaux <guillaume@heroes.jobs>
2022-05-27 11:03:14 +0200

    Initial Commit

---
+++ Now/NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowApp.swift
+//  Now
+//
+//  Created by Guillaume Bisiaux on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
